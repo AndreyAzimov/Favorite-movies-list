90c23e13a678e33da0a18e97687615ffe7ceddd1
Andrey Azimov <azimov.andrey@gmail.com>
2016-09-03 11:33:11 +0800

    init commit

---
+++ media.py
@@ -0,0 +1,17 @@
+import webbrowser
+# Importing python library webbrowser
+
+
+class Movie():
+    """This Class provides a constructor method to store movie details \
+    and a method to open the youtube video in a browser"""
+    def __init__(
+      self,
+      movie_title, movie_storyline, poster_image, trailer_youtube):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
---
+++ movies_list.py
@@ -0,0 +1,224 @@
+import media
+import page_generator
+# Importing media and fresh_tomatoes libraries
+
+# This file stores the movie information. (Title, Image URL, Youtube URL)
+
+forgotten = media.Movie(
+    "The Forgotten",
+    "a",
+    "https://upload.wikimedia.org/wikipedia/en/7/73/The_Forgotten_poster.JPG",
+    "https://www.youtube.com/watch?v=ZgiR3DECgjM")
+
+knowing = media.Movie(
+    "Knowing",
+    "a",
+    "https://upload.wikimedia.org/wikipedia/en/0/08/Knowingposter08.jpg",
+    "https://www.youtube.com/watch?v=RQE-rB3Nkh8")
+
+dracula = media.Movie(
+    "Bram Stoker's Dracula",
+    "a",
+    "https://upload.wikimedia.org/wikipedia/en/d/df/Dracpos.jpg",
+    "https://www.youtube.com/watch?v=QMvD2Jlyy9U")
+
+mirrors = media.Movie(
+    "Mirrors",
+    "a",
+    "https://upload.wikimedia.org/wikipedia/en/f/f1/Mirrorsposter08.jpg",
+    "https://www.youtube.com/watch?v=9tbr865kv3o")
+
+the1408 = media.Movie(
+    "1408",
+    "a",
+    "https://upload.wikimedia.org/wikipedia/en/6/63/1408poster.jpg",
+    "https://www.youtube.com/watch?v=WIASqPZqnhs")
+
+the1408 = media.Movie(
+    "1408",
+    "a",
+    "https://upload.wikimedia.org/wikipedia/en/6/63/1408poster.jpg",
+    "https://www.youtube.com/watch?v=WIASqPZqnhs")
+
+prestige = media.Movie(
+    "Prestige",
+    "a",
+    "https://upload.wikimedia.org/wikipedia/en/d/d2/Prestige_poster.jpg",
+    "https://www.youtube.com/watch?v=o4gHCmTQDVI")
+
+vidocq = media.Movie(
+    "Vidocq",
+    "a",
+    "https://upload.wikimedia.org/wikipedia/en/4/49/Vidocq2001.jpg",
+    "https://www.youtube.com/watch?v=JscJN3Q-W7Q")
+
+game = media.Movie(
+    "The Game",
+    "a",
+    "https://upload.wikimedia.org/wikipedia/en/5/53/TheGame_poster323.jpg",
+    "https://www.youtube.com/watch?v=dLauqDChQGs")
+
+skeleton_key = media.Movie(
+    "The Skeleton Key",
+    "a",
+    "https://upload.wikimedia.org/wikipedia/en/0/05/Skeleton_key.jpg",
+    "https://www.youtube.com/watch?v=654uzrc6Lsk")
+
+others = media.Movie(
+    "The Others",
+    "a",
+    "https://upload.wikimedia.org/wikipedia/en/4/4c/TheOthers.jpg",
+    "https://www.youtube.com/watch?v=0bMEGtUxajY")
+
+from_hell = media.Movie(
+    "From Hell",
+    "a",
+    "https://upload.wikimedia.org/wikipedia/en/6/66/From_Hell_film.jpg",
+    "https://www.youtube.com/watch?v=8-UL3V15E8s")
+
+ninth_gate = media.Movie(
+    "The Ninth Gate",
+    "a",
+    "https://upload.wikimedia.org/wikipedia/en/0/03/Ninth_gate_ver3.jpg",
+    "https://www.youtube.com/watch?v=IKc1n-nIIx0")
+
+angel_heart = media.Movie(
+    "Angel Heart",
+    "a",
+    "https://upload.wikimedia.org/wikipedia/en/1/1c/Angel_Heart.jpg",
+    "https://www.youtube.com/watch?v=DTC9Lt3hiWo")
+
+devils_advocate = media.Movie(
+    "Devil's Advocate",
+    "a",
+    "https://upload.wikimedia.org/wikipedia/en/3/3f/Devilsadvocate.jpg",
+    "https://www.youtube.com/watch?v=IBxD1qZnJzw")
+
+eneny = media.Movie(
+    "Enemy",
+    "a",
+    "https://upload.wikimedia.org/wikipedia/en/0/0d/Enemy_poster.jpg",
+    "https://www.youtube.com/watch?v=FJuaAWrgoUY")
+
+prisoners = media.Movie(
+    "Prisoners",
+    "a",
+    "https://upload.wikimedia.org/wikipedia/en/6/63/Prisoners2013Poster.jpg",
+    "https://www.youtube.com/watch?v=bpXfcTF6iVk")
+
+k_pax = media.Movie(
+    "K-PAX",
+    "a",
+    "https://upload.wikimedia.org/wikipedia/ru/2/2e/KPAX.jpg",
+    "https://www.youtube.com/watch?v=ryBxYXeu55k")
+
+peaceful_warrior = media.Movie(
+    "Peaceful Warrior",
+    "a",
+    "https://upload.wikimedia.org/wikipedia/en/thumb/9/9c/Peaceful_warrior.jpg/430px-Peaceful_warrior.jpg",
+    "https://www.youtube.com/watch?v=gegNMYvY_yg")
+
+peaceful_warrior = media.Movie(
+    "Peaceful Warrior",
+    "a",
+    "https://upload.wikimedia.org/wikipedia/en/thumb/9/9c/Peaceful_warrior.jpg/430px-Peaceful_warrior.jpg",
+    "https://www.youtube.com/watch?v=gegNMYvY_yg")
+
+interstate_60 = media.Movie(
+    "Interstate 60",
+    "a",
+    "https://upload.wikimedia.org/wikipedia/en/d/db/Interstate_60%272002.jpg",
+    "https://www.youtube.com/watch?v=j1YiQGf9eBQ")
+
+lost = media.Movie(
+    "Lost",
+    "a",
+    "http://ia.media-imdb.com/images/M/MV5BMjA3NzMyMzU1MV5BMl5BanBnXkFtZTcwNjc1ODUwMg@@._V1_.jpg",
+    "https://www.youtube.com/watch?v=A8Mbum8rheg")
+
+lost_room = media.Movie(
+    "Lost Room",
+    "a",
+    "https://upload.wikimedia.org/wikipedia/en/3/3c/Thelostroom.jpg",
+    "https://www.youtube.com/watch?v=sZOZypQWSu8")
+
+booth = media.Movie(
+    "The Booth at the End",
+    "a",
+    "https://upload.wikimedia.org/wikipedia/en/2/2b/The_Booth_at_the_End.jpg",
+    "https://www.youtube.com/watch?v=hLyzvc8QHL0")
+
+silicon_valley = media.Movie(
+    "Silicon Valley",
+    "a",
+    "http://ia.media-imdb.com/images/M/MV5BMTgwNTUzNzIxM15BMl5BanBnXkFtZTgwMzQ1NTk2ODE@._V1_SY1000_CR0,0,674,1000_AL_.jpg",
+    "hhttps://www.youtube.com/watch?v=69V__a49xtw")
+
+social_network = media.Movie(
+    "Social Network",
+    "a",
+    "https://upload.wikimedia.org/wikipedia/en/7/7a/Social_network_film_poster.jpg",
+    "https://www.youtube.com/watch?v=2RB3edZyeYw")
+
+office_space = media.Movie(
+    "Office Space",
+    "a",
+    "https://upload.wikimedia.org/wikipedia/en/8/8e/Office_space_poster.jpg",
+    "https://www.youtube.com/watch?v=kwQziVIzDeg")
+
+boiler_room = media.Movie(
+    "Boiler Room",
+    "a",
+    "https://upload.wikimedia.org/wikipedia/en/5/5c/Boiler_room_ver3.jpg",
+    "https://www.youtube.com/watch?v=6VoXMvNrQro")
+
+wall_street = media.Movie(
+    "Wall Street Film",
+    "a",
+    "https://upload.wikimedia.org/wikipedia/en/b/bc/Wall_Street_film.jpg",
+    "https://www.youtube.com/watch?v=7b4BcbhGggM")
+
+glen_ross = media.Movie(
+    "Glengarry Glen Ross",
+    "a",
+    "https://upload.wikimedia.org/wikipedia/en/thumb/4/49/Glengarry_Glen_Ross_Poster.jpg/440px-Glengarry_Glen_Ross_Poster.jpg",
+    "https://www.youtube.com/watch?v=KUqo5tPZjrM")
+
+eight_mile = media.Movie(
+    "8 Mile",
+    "a",
+    "https://upload.wikimedia.org/wikipedia/en/8/8b/Eight_mile_ver2.jpg",
+    "https://www.youtube.com/watch?v=axGVrfwm9L4")
+
+intouchables = media.Movie(
+    "Intouchables",
+    "a",
+    "https://upload.wikimedia.org/wikipedia/en/9/93/The_Intouchables.jpg",
+    "https://www.youtube.com/watch?v=34WIbmXkewU")
+
+cat_with_hands = media.Movie(
+    "The Cat With Hands",
+    "a",
+    "https://www.kinopoisk.ru/images/film_big/109015.jpg",
+    "https://www.youtube.com/watch?v=n4PR9NZlAB4")
+
+vincent = media.Movie(
+    "Vincent",
+    "a",
+    "http://ia.media-imdb.com/images/M/MV5BNjhlMjJmZDUtNTQ4NS00NjI4LWJiYzAtYTkxOTI5MDRhMGEzXkEyXkFqcGdeQXVyMjM0MTE3ODQ@._V1_SY1000_CR0,0,733,1000_AL_.jpg",
+    "https://www.youtube.com/watch?v=fxQcBKUPm8o")
+
+
+movies = [lost, lost_room, booth, peaceful_warrior, forgotten,
+          knowing, dracula, mirrors, the1408, prestige, vidocq, game, skeleton_key, others,
+          from_hell, ninth_gate, angel_heart, devils_advocate, cat_with_hands, vincent, prisoners,
+          eneny, k_pax, wall_street, eight_mile, boiler_room, glen_ross, office_space, social_network,
+          silicon_valley]
+
+
+# Calling open_movies_page definition from fresh_tomatoes library.
+# This definition will create a static webpage for the list of movies
+
+print ("test")
+
+page_generator.open_movies_page(movies)
